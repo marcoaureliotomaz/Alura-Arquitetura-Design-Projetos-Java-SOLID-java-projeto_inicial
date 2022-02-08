00b07937b92570ec7e843941b3cd362c7beeaf12
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-02-08 00:29:15 -0300

    First Commit

--- src/Application.java
+++ src/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) {
 
         Funcionario f = new Funcionario("Marco","00801618479", Cargo.ANALISTA, BigDecimal.valueOf(15000.00));
-        System.out.println(f);
+
+
     }
 }
